c14681957c0ca130563b49f895f2c8b427404528
Collin Page <cpage.web@gmail.com>
2023-06-24 13:49:50 -0400

    Added typo plugin and styled home (WIP)

--- src/App.js
+++ src/App.js
@@ -7,46 +7,56 @@ function App() {
   const { t } = useTranslation('home');
 
   return (
-    <div className="App bg-black flex flex-col min-h-full">
-      <header className="App-header sticky top-0 flex item-center justify-between p-3 flex-0 z-50 bg-white">
-        <h1>CP</h1>
+    <div className="App flex flex-col min-h-full">
+      <header className="App-header sticky top-0 border-b-2 border-slate-50 p-3 flex-0 z-50 bg-white shadow">
+        <div className="container flex items-center justify-between">
+          <p className="text-5xl">CP</p>
 
-        <nav>
-          <ul className="flex item-center gap-x-3">
-            <li>
-              <a href="#about">{t('menu.about')}</a>
-            </li>
+          <nav>
+            <ul className="flex items-center gap-x-3">
+              <li>
+                <a href="#about" className="hover:underline text-lg">{t('menu.about')}</a>
+              </li>
 
-            <li>
-              <a href="#work">{t('menu.work')}</a>
-            </li>
+              <li>
+                <a href="#work" className="hover:underline text-lg">{t('menu.work')}</a>
+              </li>
 
-            <li>
-              <a href="#skills">{t('menu.skills')}</a>
-            </li>
+              <li>
+                <a href="#skills" className="hover:underline text-lg">{t('menu.skills')}</a>
+              </li>
 
-            <li>
-              {i18next.language === 'fr' ? (
-                <button onClick={() => i18next.changeLanguage('en')}>EN</button>
-              ) : (
-                <button onClick={() => i18next.changeLanguage('fr')}>FR</button>
-              )}
-            </li>
-          </ul>
-        </nav>
+              <li>
+                {i18next.language === 'fr' ? (
+                  <button onClick={() => i18next.changeLanguage('en')} className="hover:underline text-lg">EN</button>
+                ) : (
+                  <button onClick={() => i18next.changeLanguage('fr')}>FR</button>
+                )}
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
 
       <main className="flex-grow min-h-full">
-        <h1 className="text-white text-9xl">
-          {t('presentation')} <span>Collin</span>
-        </h1>
+        <section className="h-screen flex items-center">
+          <div className="container flex gap-10 items-center">
+            <h1 className="prose text-9xl w-3/5">
+              {t('presentation')} <span className="text-teal-800">Collin</span>
+            </h1>
+
+            <div className="w-2/5">
+              <p>Image</p>
+            </div>
+          </div>
+        </section>
 
       </main>
 
       <footer className="flex-0">
         Copyright Collin Pagé
       </footer>
-    </div>
+    </div >
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,43 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}",],
   theme: {
-    extend: {},
+    // screens: {
+    //   sm: '480px',
+    //   md: '768px',
+    //   lg: '976px',
+    //   xl: '1440px',
+    // },
+    // colors: {
+    //   'blue': '#1fb6ff',
+    //   'purple': '#7e5bef',
+    //   'pink': '#ff49db',
+    //   'orange': '#ff7849',
+    //   'green': '#13ce66',
+    //   'yellow': '#ffc82c',
+    //   'gray-dark': '#273444',
+    //   'gray': '#8492a6',
+    //   'gray-light': '#d3dce6',
+    // },
+    fontFamily: {
+      sans: ['Bebas Neue', 'sans-serif'],
+      serif: ['Heebo', 'serif'],
+    },
+    container: {
+      center: true
+    },
+    extend: {
+      height: {
+        screen: 'calc(100vh - 62px)' // Change .h-screen to remove sticky header height
+      }
+      // spacing: {
+      //   '128': '32rem',
+      //   '144': '36rem',
+      // },
+      // borderRadius: {
+      //   '4xl': '2rem',
+      // }
+    }
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
 
